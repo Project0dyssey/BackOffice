f7b6efefa527d34ca36f5a848a9819a8d85fe162
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-03-16 15:19:10 +0000

    created endpoint for the site

---
+++ pages/api/v1/site.ts
@@ -0,0 +1,12 @@
+import { FilterProducts } from "@/mongodb/curd";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async(req: NextApiRequest, res: NextApiResponse) => {
+  const {category, collection} = req.body
+    try {
+        const result = await FilterProducts(category, collection)
+        res.status(200).json({result: result})
+    } catch (error) {
+        res.status(400).json({result: 'Error'})
+    }
+}
