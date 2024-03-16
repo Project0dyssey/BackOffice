dcfbff8e9ded8b7ac150d8bb64ad40464fb54eeb
Daniel Bárbara <daniel.santos.barbara@gmail.com>
2024-03-16 15:51:41 +0000

    testing

--- next.config.mjs
+++ next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
             source: "/api/v1/site",
             headers: [
                 { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "true" }, // replace this your actual origin
+                { key: "Access-Control-Allow-Origin", value: "POST" }, // replace this your actual origin
                 { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
             ]
