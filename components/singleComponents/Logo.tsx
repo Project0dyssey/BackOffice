dcccd9ae4c967fe44ced2bfacf9f8d05cba01e21
Ana Isa Pereira Nogueira <139065337+AnaIsaPN@users.noreply.github.com>
2024-03-30 12:06:53 +0000

    Logo fix

--- components/singleComponents/Logo.tsx
+++ components/singleComponents/Logo.tsx
@@ -1,7 +1,7 @@
 export function Logo(){
     return(
         <>
-            <img src="./images/Logo.png" alt="Organic Mean Logo" className="h-24"/>
+            <img src="./images/Logo.svg" alt="WebHopper Logo" className="h-24"/>
         </>
     )
 }
