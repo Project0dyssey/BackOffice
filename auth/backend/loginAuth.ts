9dd0c39fa9911622326fbfcfb1caaf2ff61ca459
Ana Isa Pereira Nogueira <139065337+AnaIsaPN@users.noreply.github.com>
2024-04-24 11:00:55 +0100

    Alterações do Rafa

--- auth/backend/loginAuth.ts
+++ auth/backend/loginAuth.ts
@@ -1,16 +1,25 @@
-import { GetUserInfo } from "@/mongodb/crud"
-import { ObjectId } from "mongodb"
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
+import { GetUserInfo } from "@/mongodb/crud";
+import { ObjectId } from "mongodb";
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 export async function login(email: string, password: string) {
-    const user = await GetUserInfo(email)
-    if(!user) return false
-    const verifyPw = await bcrypt.compare(password, user.password)
-    return await verifyPw ? genJwtToken(user._id) : false
+  console.log(0);
+  const user = await GetUserInfo(email);
+  console.log(1);
+  console.log(user);
+  if (!user) return false;
+  console.log(2);
+  const verifyPw = await bcrypt.compare(password, user.password);
+  console.log(3);
+  const token = await genJwtToken(user._id);
+  console.log(token);
+  return (await verifyPw) ? token : false;
 }
 
- async function genJwtToken(userId: ObjectId) {
-    const jwtExpire: string = process.env.JWT_EXPIRES!
-    return await jwt.sign({_id: userId}, process.env.JWT_SECRET, {expiresIn: jwtExpire})
+async function genJwtToken(userId: ObjectId) {
+  const jwtExpire: string = process.env.JWT_EXPIRES!;
+  return await jwt.sign({ _id: userId }, process.env.JWT_SECRET, {
+    expiresIn: jwtExpire,
+  });
 }
--- pages/products.tsx
+++ pages/products.tsx
@@ -1,51 +1,79 @@
-import { CheckBox } from "@/components/singleComponents/CheckBox"
-import { ProductCard } from "@/components/singleComponents/ProductCard"
-import { SearchInputs } from "@/components/singleComponents/SearchInputs"
-import { GetToken } from "@/logic/frontend/auth"
-import { products } from "@/logic/frontend/fetchs"
-import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
-
+import { CheckBox } from "@/components/singleComponents/CheckBox";
+import { ProductCard } from "@/components/singleComponents/ProductCard";
+import { SearchInputs } from "@/components/singleComponents/SearchInputs";
+import { GetToken } from "@/logic/frontend/auth";
+import { products } from "@/logic/frontend/fetchs";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { DataTable, TProduct } from "./yabadabadu";
+import { ColumnDef } from "@tanstack/react-table";
 
 interface filterType {
-    category: string
-    collection: Array<string>
+  category: string;
+  collection: Array<string>;
 }
 
 interface product {
-    name: string
-    nameEng: string
+  name: string;
+  nameEng: string;
 }
 
+const columns: ColumnDef<any>[] = [
+  {
+    accessorKey: "productNameEng",
+    header: "Name",
+  },
+  {
+    accessorKey: "collection",
+    header: "Collection",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+];
+
 export default function Products() {
-    const [filter, setFilter] = useState<filterType>({ category: 'All', collection: [] })
-    const [product, setProduct] = useState<Array<product>>([{ name: '', nameEng: '' }])
-    const router = useRouter()
+  const [filter, setFilter] = useState<filterType>({
+    category: "All",
+    collection: [],
+  });
+  const [product, setProduct] = useState<Array<product>>([
+    { name: "", nameEng: "" },
+  ]);
+  const router = useRouter();
 
-    useEffect(() => {
-        async function getLoggedIn() {
-            const getToken = await GetToken()
-            if (!getToken) return router.push('/')
+  useEffect(() => {
+    async function getLoggedIn() {
+      const getToken = await GetToken();
+      if (!getToken) return router.push("/");
 
-            const fetchProducts = await products(filter)
-            setProduct(fetchProducts)
-        }
-        getLoggedIn()
-    }, [filter])
-    
-    return (
-        <div className="flex flex-col gap-5 h-dvh items-center overflow-scroll">
-            <div className="flex mt-2 text-[1.3rem]">
-                <p className="text-yellow-500">P</p><p className="text-white">rodutos</p>
-            </div>
-            <SearchInputs setFilter={setFilter}/>
-            <div className="w-[100%] flex flex-col items-center p-5">
-                {product.map((el: any, i: number) =>
-                    <div key={i} className="w-[100%] flex justify-center">
-                        <ProductCard productInfo={el} />
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+      const fetchProducts = await products(filter);
+      setProduct(fetchProducts);
+    }
+    getLoggedIn();
+  }, [filter]);
+
+  console.log(product);
+
+  return (
+    <div className="flex flex-col gap-5 h-dvh items-center overflow-scroll">
+      <div className="flex mt-2 text-[1.3rem]">
+        <p className="text-yellow-500">P</p>
+        <p className="text-white">rodutos</p>
+      </div>
+      <SearchInputs setFilter={setFilter} />
+      <div className="w-[100%] flex flex-col items-center p-5">
+        {product.map((el: any, i: number) => (
+          <div key={i} className="w-[100%] flex justify-center">
+            <ProductCard productInfo={el} />
+          </div>
+        ))}
+      </div>
+
+      <div className="w-4/5 bg-zinc-950 rounded-md text-white">
+        <DataTable columns={columns} data={product} />
+      </div>
+    </div>
+  );
 }
